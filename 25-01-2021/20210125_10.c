2ea468bbb8f918d5de45a2365454060e127d9665
Emma1001 <57152142+Emma1001@users.noreply.github.com>
2021-01-26 14:31:47 +0200

    Create 20210125_10.c

---
+++ 25-01-2021/20210125_10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void reverse(char text[], int count){
+    for (int i = count; i >= 0; i--)
+    {
+        printf("%c", text[i]);
+    }
+
+    printf("\n");
+}
+
+int main(void){
+    int c;
+    char text[255];
+    int index = 0;
+
+    while ((c = getchar()) != EOF){
+        text[index] = c;
+        index++;
+
+        if(c=='\n'){
+            reverse(text, index);
+            index = 0;
+        }
+    }
+
+    return 0;
+}
